43a8653e1c5b5f9df22e8b9154fb3bb4e277e7e9
ashish32patel <ashish32patel@gmail.com>
2023-09-29 01:04:23 +0530

    Inserting Data Into the Database

--- models/product.js
+++ models/product.js
@@ -11,7 +11,10 @@ module.exports = class Product {
   }
 
   save() {
-
+    return db.execute(       //returns the promise that execute yields
+      'INSERT INTO products (title,price,imageUrl,description) VALUES (?,?,?,?)',
+      [this.title, this.price, this.imageUrl, this.description]
+    );
   }
 
   static deleteById(id) {
@@ -19,7 +22,7 @@ module.exports = class Product {
   }
 
   static fetchAll() {
-    return db.execute("SELECT * FROM products"); //returns a promise
+    return db.execute("SELECT * FROM products"); //returns the promise that execute yields
   }
 
   static findById(id, cb) {
